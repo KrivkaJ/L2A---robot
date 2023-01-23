fcfca4765617411e5c0568d1233b05b7c87481eb
JakubSlof <123386172+JakubSlof@users.noreply.github.com>
2023-01-23 16:23:37 +0100

    ymena

--- motors/src/main.cpp
+++ motors/src/main.cpp
@@ -4,12 +4,10 @@
  float turn = 127.45; //vzdalenost co ma kazde kolo ujet pro otoceni o 90°
 
  void turn_left(){
-        rkMotorsDriveLeft  (-turn,speed);
-        rkMotorsDriveRight (turn,speed); 
+       rkMotorsDriveAsync(turn,-turn,speed);
 }
     void turn_right(){
-        rkMotorsDriveLeft  (turn,speed);
-        rkMotorsDriveRight (-turn,speed); 
+        rkMotorsDriveAsync(-turn,turn,speed);
     }
 
 // Funkce setup se zavolá vždy po startu robota.
@@ -19,29 +17,22 @@ void setup() {
     // cfg.motor_max_power_pct = 30; // limit výkonu motorů na 30%
     rkSetup(cfg);          
     while(true){
-        
-        rkMotorsDriveLeft  (950,speed);
-        rkMotorsDriveRight (950,speed);
+        rkMotorsDriveAsync(950,950,speed);
 
          turn_right();
     
-          rkMotorsDriveLeft (300,speed);
-          rkMotorsDriveRight (300,speed);
+          rkMotorsDriveAsync(300,300,speed);
          
            turn_right();
 
-             rkMotorsDriveLeft (500,speed);
-             rkMotorsDriveRight (500,speed);
-         
+          rkMotorsDriveAsync(500,500,speed);
               turn_left();
 
-               rkMotorsDriveLeft (500,speed);
-               rkMotorsDriveRight (500,speed);
+               rkMotorsDriveAsync(500,500,speed);
          
                 turn_left();
         
-                 rkMotorsDriveLeft (1000,speed);
-                 rkMotorsDriveRight (1000,speed);
+               rkMotorsDriveAsync(1000,1000,speed);
          
     }
 }
