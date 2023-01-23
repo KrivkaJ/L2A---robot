a4403aedf840d06ac5afe2e32791aac67512cf30
JakubSlof <123386172+JakubSlof@users.noreply.github.com>
2023-01-23 18:51:19 +0100

    oprava programu

--- motors/src/main.cpp
+++ motors/src/main.cpp
@@ -1,7 +1,8 @@
 
 #include "robotka.h"
- int speed = 100;
- float turn = 127.45; //vzdalenost co ma kazde kolo ujet pro otoceni o 90°
+
+int speed = 100;
+float turn = 127.45; //vzdalenost co ma kazde kolo ujet pro otoceni o 90°
 
  void turn_left(){
        rkMotorsDriveAsync(turn,-turn,speed);
@@ -16,23 +17,33 @@ void setup() {
     // Upravte nastavení, například:
     // cfg.motor_max_power_pct = 30; // limit výkonu motorů na 30%
     rkSetup(cfg);          
-    while(true){
+
+
         rkMotorsDriveAsync(950,950,speed);
+         delay(1);
 
          turn_right();
-    
-          rkMotorsDriveAsync(300,300,speed);
-         
-           turn_right();
+          delay(1);
 
-          rkMotorsDriveAsync(500,500,speed);
+          rkMotorsDriveAsync(300,300,speed);
+           delay(1);
+
+           turn_right();
+            delay(1);
+
+            rkMotorsDriveAsync(500,500,speed);
+             delay(1);
+
               turn_left();
+               delay(1);
 
                rkMotorsDriveAsync(500,500,speed);
-         
+                delay(1);
+
                 turn_left();
-        
-               rkMotorsDriveAsync(1000,1000,speed);
-         
-    }
+                 delay(1);
+
+                 rkMotorsDriveAsync(1000,1000,speed);
+                  delay(1);
+    
 }
