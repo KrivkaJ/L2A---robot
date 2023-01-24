0c94cc24890be364287ce0534c67d8f7c20c84d1
KrivkaJ <92040073+KrivkaJ@users.noreply.github.com>
2023-01-24 15:49:01 +0100

    přidá základní příkazy na motory

--- motors/src/main.cpp
+++ motors/src/main.cpp
@@ -2,15 +2,25 @@
 #include "robotka.h"
 
 int speed = 50;
-float turn = 127.45; //vzdalenost co ma kazde kolo ujet pro otoceni o 90°
+float coefSpeed = 1.085; // pravy motor je pomalejsi, takze se jeho rychlost musi nasobit touto konstantou 
+float ticksToMm = 4.65; // prepocet z tiku v enkoderech na mm 
+byte state = 1;
+float rozpeti_kol = 173;
 
- void turn_left(){
-       rkMotorsDriveAsync(turn*5,-turn*5,speed);
+
+void forward(int mm, byte state_x){
+    rkConfig cfg;
+    rkSetup(cfg); 
+        rkMotorsDriveLeftAsync(mm*ticksToMm, speed);
+        rkMotorsDriveRightAsync(mm*ticksToMm, speed);
 }
-    void turn_right(){
-        rkMotorsDriveAsync(-turn*5,turn*5,speed);
-    }
 
+void turn(int degrees, byte state_x){
+    rkConfig cfg;
+    rkSetup(cfg);
+        rkMotorsDriveLeftAsync(3.141*rozpeti_kol*degrees/360*ticksToMm, speed);
+        rkMotorsDriveRightAsync(3.141*rozpeti_kol*degrees/360*ticksToMm, speed);
+}
 // Funkce setup se zavolá vždy po startu robota.
 void setup() {
     rkConfig cfg;
@@ -18,31 +28,55 @@ void setup() {
     // cfg.motor_max_power_pct = 30; // limit výkonu motorů na 30%
     rkSetup(cfg);          
 
-        rkMotorsDriveAsync(950*5,950*5,speed);
-         delay(8000);
+    while(true){
+        switch(state){
+        case 1:
+            state = 2;
+                forward(450, 3);
+            break;
 
-         turn_right();
-          delay(5000);
+        case 3: // otocka vpravo
+            state = 4;
+            turn(-90, 5);
+            break;
 
-          rkMotorsDriveAsync(300*5,300*5,speed);
-           delay(5000);
+        case 5:
+            state = 6;
+                forward(450, 7);
+            break;
 
-           turn_right();
-            delay(5000);
+        case 7:
+            state = 8;
+                turn(-90, 9);
+            break;
 
-            rkMotorsDriveAsync(500*5,500*5,speed);
-             delay(5000);
+        case 9:
+            state = 10;
+                forward(500, 11);
+            break;
 
-              turn_left();
-               delay(5000);
+        case 11:
+            state = 12;
+                turn(90, 13);
+            break;
 
-               rkMotorsDriveAsync(500,500,speed);
-                delay(5000);
+        case 13:
+            state = 14;
+                forward(500, 15);
+            break;
 
-                turn_left();
-                 delay(5000);
+        case 15:
+            state = 16;
+                turn(90, 17);
+            break;
 
-                 rkMotorsDriveAsync(1000,1000,speed);
-                  delay(5000);
-    
+        case 17:
+            state = 18;
+                forward(1000, 19);
+            break;
+
+        default:
+            break;
+        }
+    }
 }
