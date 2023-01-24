27018c3a0b32a4c2df1866acf1afdbc5c6dd793a
KrivkaJ <92040073+KrivkaJ@users.noreply.github.com>
2023-01-24 16:09:02 +0100

    přidá tlačítko

--- motors/src/main.cpp
+++ motors/src/main.cpp
@@ -8,75 +8,30 @@ byte state = 1;
 float rozpeti_kol = 173;
 
 
-void forward(int mm, byte state_x){
+void forward(int mm){ // 
     rkConfig cfg;
     rkSetup(cfg); 
-        rkMotorsDriveLeftAsync(mm*ticksToMm, speed);
-        rkMotorsDriveRightAsync(mm*ticksToMm, speed);
+        rkMotorsDrive(mm*ticksToMm, mm*ticksToMm, speed);
 }
 
-void turn(int degrees, byte state_x){
+void turn(int degrees){
     rkConfig cfg;
     rkSetup(cfg);
-        rkMotorsDriveLeftAsync(3.141*rozpeti_kol*degrees/360*ticksToMm, speed);
-        rkMotorsDriveRightAsync(3.141*rozpeti_kol*degrees/360*ticksToMm, speed);
+        rkMotorsDrive(3.141*rozpeti_kol*degrees/360*ticksToMm, -3.141*rozpeti_kol*degrees/360*ticksToMm, speed);
 }
+
 // Funkce setup se zavolá vždy po startu robota.
 void setup() {
     rkConfig cfg;
     // Upravte nastavení, například:
     // cfg.motor_max_power_pct = 30; // limit výkonu motorů na 30%
     rkSetup(cfg);          
-
     while(true){
-        switch(state){
-        case 1:
-            state = 2;
-                forward(450, 3);
-            break;
-
-        case 3: // otocka vpravo
-            state = 4;
-            turn(-90, 5);
-            break;
-
-        case 5:
-            state = 6;
-                forward(450, 7);
-            break;
-
-        case 7:
-            state = 8;
-                turn(-90, 9);
-            break;
-
-        case 9:
-            state = 10;
-                forward(500, 11);
-            break;
-
-        case 11:
-            state = 12;
-                turn(90, 13);
-            break;
-
-        case 13:
-            state = 14;
-                forward(500, 15);
-            break;
-
-        case 15:
-            state = 16;
-                turn(90, 17);
-            break;
-
-        case 17:
-            state = 18;
-                forward(1000, 19);
-            break;
-
-        default:
-            break;
+        if(rkButtonIsPressed(BTN_UP)){
+                break;
         }
     }
+
+    forward(1000);
+
 }
